9c4a57112e9fb7bccbf7cd5f1d0e2db453ab526d
Daniel Wabyick <dwabyick@adobe.com>
2014-01-03 14:01:53 -0800

    adding link install script and solarized.vim

---
+++ link.sh
@@ -0,0 +1,8 @@
+ln -sf $PWD/aliases ~/.aliases
+ln -sf $PWD/bash_profile ~/.bash_profile
+ln -sf $PWD/bashrc ~/.bashrc
+ln -sf $PWD/git-completion.bash ~/.git-completion.bash
+ln -sf $PWD/gitconfig ~/.gitconfig
+ln -sf $PWD/gitignore_global ~/.gitignore
+ln -sf $PWD/vimrc ~/.vimrc
+
