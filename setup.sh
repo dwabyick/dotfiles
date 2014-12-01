b07c34629f8c653e18553c97e263fa5b254eb47f
Daniel Wabyick <dwabyick@adobe.com>
2014-11-30 18:53:39 -0800

    added bash_path default

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 # Setting up the dotfiles for a new machine.
 ln -sf $PWD/aliases ~/.aliases
 ln -sf $PWD/bash_profile ~/.bash_profile
+ln -sf $PWD/bash_path ~/.bash_path
 ln -sf $PWD/bashrc ~/.bashrc
 ln -sf $PWD/git-completion.bash ~/.git-completion.bash
 ln -sf $PWD/gitconfig ~/.gitconfig
